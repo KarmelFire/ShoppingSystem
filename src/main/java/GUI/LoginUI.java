748a96ba70bdce75619803072171789e211b2446
bc <bian2003chen@163.com>
2023-03-20 20:31:23 +0800

    登录界面UI完成

--- src/main/java/GUI/LoginUI.java
+++ src/main/java/GUI/LoginUI.java
@@ -1,21 +1,58 @@
 package GUI;
 
+import Utiltool.GuiUtil.getMiddlelocation;
+
 import javax.swing.*;
+import java.awt.*;
+
 
 public class LoginUI extends JFrame {
-    public LoginUI(String title) {
-        super(title);
+    public static void showlogin() {
+        JFrame login = new JFrame();
+        login.setSize(400, 300);
+        login.setTitle("账户登录");
+        login.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+//        容器设置
         JPanel root = new JPanel();
-        this.setContentPane(root);
+        login.setContentPane(root);
+        login.setResizable(false);
         root.setLayout(null);
 
+//        输入用户名
+        JLabel act = new JLabel("用户名");
+        act.setBounds(60, 40, 50, 30);
+        root.add(act);
 
-        root.add (new JLabel("账号"));
-        root.add (new JTextField(15));
+        JTextField Act = new JTextField();
+        Act.setBounds(130, 40, 150, 25);
+        root.add(Act);
 
-        root.add (new JLabel("密码"));
-        root.add (new JTextField(15));
+//        输入密码
+        JLabel psd = new JLabel("密码");
+        psd.setBounds(60, 100, 50, 30);
+        root.add(psd);
 
+        JTextField Psd = new JTextField();
+        Psd.setBounds(130, 100, 150, 25);
+        root.add(Psd);
+
+//        登录按钮
+        JButton button = new JButton("登录");
+        button.setBounds(145, 160, 120, 30);
+        root.add(button);
+
+        JLabel label = new JLabel("点击注册账号");
+        label.setBounds(173, 190, 100, 30);
+        Font font = new Font("微软雅黑", Font.PLAIN, 10);
+        label.setFont(font);
+        root.add(label);
+
+//        窗口位置居中
+        int[] location = new getMiddlelocation().getMiddlelocation(login);
+        login.setLocation(location[0], location[1]);
+
+//        login.setVisible(true);
     }
 }
+;
--- src/test/java/GUI/LoginUITest.java
+++ src/test/java/GUI/LoginUITest.java
@@ -6,10 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoginUITest {
     public static void main(String[] args) {
-        LoginUI loginUI = new LoginUI("账户登录");
-        loginUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loginUI.setSize(400, 300);
-        loginUI.setVisible(true);
+        LoginUI.showlogin();
     }
 
 }
