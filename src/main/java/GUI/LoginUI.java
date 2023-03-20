1e47bfac4de4f53780ca74370410b9c3ece709c4
bc <bian2003chen@163.com>
2023-03-20 18:21:59 +0800

    推送测试2

--- src/main/java/GUI/LoginUI.java
+++ src/main/java/GUI/LoginUI.java
@@ -2,8 +2,20 @@ package GUI;
 
 import javax.swing.*;
 
-public class LoginUI extends JPanel {
+public class LoginUI extends JFrame {
     public LoginUI(String title) {
+        super(title);
+
+        JPanel root = new JPanel();
+        this.setContentPane(root);
+        root.setLayout(null);
+
+
+        root.add (new JLabel("账号"));
+        root.add (new JTextField(15));
+
+        root.add (new JLabel("密码"));
+        root.add (new JTextField(15));
 
     }
 }
