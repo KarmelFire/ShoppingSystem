7a1465281d3357b2bc306e4c130180acadf93535
Langxecho <1908630172@qq.com>
2023-03-26 20:05:42 +0800

    部分静态方法修改

--- src/main/java/GUI/GoodsetUI.java
+++ src/main/java/GUI/GoodsetUI.java
@@ -5,20 +5,56 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import Utiltool.GuiUtil.rightMenu;
+import Utiltool.dbUtil.dbUtil;
 
 public class GoodsetUI extends JPanel {
+    JTable table;
     public GoodsetUI() {
         setLayout(null);
 
 //        表格
         String[] columnName = {"商品", "分类", "价格", "折扣", "库存"};
-        String[][] tableValues = {{"农夫山泉", "饮料", "2", "无", "50"}, {"冰红茶", "饮料", "3", "无", "50"}};
-        JTable table = new JTable(tableValues, columnName) {
-            public boolean isCellEditable(int row, int column) {
-                return false;
+        //加载数据库
+        String sql = "select * from goods";
+        dbUtil db =new dbUtil();
+        Connection con;
+        PreparedStatement pstmt;
+        ResultSet rs;
+        String[][] tableValues;
+
+        try {
+            con = db.getConnection();
+            pstmt = con.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            rs = pstmt.executeQuery();
+            //查找数据条数
+            rs.last();
+            int rownumber = rs.getRow();
+            rs.beforeFirst();//指针归位
+            tableValues = new String[rownumber][5];//表格的行列设置
+            while (rs.next()){
+                tableValues[rs.getRow() - 1][0] = rs.getString("name");
+                tableValues[rs.getRow() - 1][1] = rs.getString("category");
+                tableValues[rs.getRow() - 1][2] = rs.getString("price");
+                tableValues[rs.getRow() - 1][3] = rs.getString("discount");
+                tableValues[rs.getRow() - 1][4] = rs.getString("store");
+                table = new JTable(tableValues, columnName) {
+                    public boolean isCellEditable(int row, int column) {
+                        return false;
+                    }
+                };
             }
-        };
+            System.out.println("表格数据加载成功");
+        }catch (Exception e){
+            System.out.println("表格数据加载失败");
+            e.printStackTrace();
+        }
+
+
         table.getTableHeader().setReorderingAllowed(false);   //不可整列移动
         table.getTableHeader().setResizingAllowed(false);   //不可拉动表格
 
